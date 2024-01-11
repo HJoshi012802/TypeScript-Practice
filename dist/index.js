38b9ad1b7b50087f8cdf3c2fc838d0eab087fdaa
Harshit Joshi <harshitjoshi2002@gmail.com>
2024-01-11 10:11:59 +0530

    Initial Commit

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+import fs from "fs";
+console.log("Run My TypeScript File");
+const addNumber = (a, b) => {
+    return a + b;
+};
+fs.writeFileSync("test.txt", "test");
+addNumber(1, 2);
+console.log("Hanji Hanji Dedo Mic pe light up hain bolna bhot kux but I swear to god this ain't that liquor talk mujhe ni karna flex but yeah bandi set hn");
+let age = "20";
---
+++ server/index.ts
@@ -0,0 +1,16 @@
+import fs from "fs"
+
+console.log("Run My TypeScript File")
+
+const addNumber =(a:number,b:number)=>{
+   return a+b;
+}
+
+fs.writeFileSync("test.txt","test")
+
+addNumber(1,2)
+
+console.log("Hanji Hanji Dedo Mic pe light up hain bolna bhot kux but I swear to god this ain't that liquor talk mujhe ni karna flex but yeah bandi set hn");
+
+let age:String ="20";
+
